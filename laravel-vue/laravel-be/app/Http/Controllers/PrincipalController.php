5074f5e9291665c0320d9b0da980874fae599170
Jeferson Junio Menchik <juniomenchik@live.com>
2022-07-31 15:44:58 -0300

    master : 30. Implementando as views principal, sobre-nos e contato

--- laravel-vue/laravel-be/app/Http/Controllers/PrincipalController.php
+++ laravel-vue/laravel-be/app/Http/Controllers/PrincipalController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class PrincipalController extends Controller
 {
     public function principal() {
-        echo 'Home';
+        return view('site.principal');
     }
 }
