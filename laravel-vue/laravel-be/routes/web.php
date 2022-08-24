9a8dbb883107c6027a2467955a5d7a0bb8a2f1e4
Jeferson Junio Menchik <juniomenchik@live.com>
2022-08-24 16:42:32 -0300

    master : 36. Agrupando rotas no app

--- laravel-vue/laravel-be/routes/web.php
+++ laravel-vue/laravel-be/routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal']);
 
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class,'sobreNos']);
@@ -21,8 +10,10 @@ Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato'
 
 Route::get('/login', function(){ return 'Login';});
 
-Route::get('/clientes', function(){ return 'Clientes';});
+// Privada
 
-Route::get('/fornecedores', function(){ return 'Fornecedores';});
-
-Route::get('/produtos',function(){ return 'Produtos';});
+Route::prefix('app')->group(function(){
+    Route::get('/clientes', function(){ return 'Clientes';});
+    Route::get('/fornecedores', function(){ return 'Fornecedores';});
+    Route::get('/produtos',function(){ return 'Produtos';});
+});
