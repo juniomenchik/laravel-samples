ff8a15ea26b1b48fee0c9f04b7a9bca1da25f834
Jeferson Junio Menchik <juniomenchik@live.com>
2022-08-24 16:51:31 -0300

    master : 37. Nomeando rotas e gerando desacoplamento no codigo

--- laravel-vue/laravel-be/routes/web.php
+++ laravel-vue/laravel-be/routes/web.php
@@ -2,18 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal']);
-
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class,'sobreNos']);
-
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato']);
-
-Route::get('/login', function(){ return 'Login';});
+Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal'])->name('site.index');
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class,'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato'])->name('site.contato');
+Route::get('/login', function(){ return 'Login';})->name('site.login');
 
 // Privada
 
 Route::prefix('app')->group(function(){
-    Route::get('/clientes', function(){ return 'Clientes';});
-    Route::get('/fornecedores', function(){ return 'Fornecedores';});
-    Route::get('/produtos',function(){ return 'Produtos';});
+    Route::get('/clientes', function(){ return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos',function(){ return 'Produtos';})->name('app.produtos');
 });
