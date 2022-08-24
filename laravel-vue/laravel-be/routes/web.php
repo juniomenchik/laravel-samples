4dc26d671fc8d9c1b4624326bf05995c59039173
Jeferson Junio Menchik <juniomenchik@live.com>
2022-08-24 12:04:56 -0300

    master : 34. Criando Menus de Navegacao

--- laravel-vue/laravel-be/routes/web.php
+++ laravel-vue/laravel-be/routes/web.php
@@ -18,11 +18,3 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal']);
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class,'sobreNos']);
 
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato']);
-
-Route::get('/contato/{nome}/{categoria_id}',
-    function(
-        string $nome = 'Desconhecido',
-        int $categoria_id = 1 // 1 - 'Informacao'
-    ){
-    echo 'Estamos Aqui: '.$nome. ' ----- '.$categoria_id;
-})->where('categoria_id','[0-9]+')->where('nome','[A-Za-z]+');
