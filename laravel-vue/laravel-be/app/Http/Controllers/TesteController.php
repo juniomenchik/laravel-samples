03f7929d2bf78720ba331694b393bbb3c6ae4f85
Jeferson Junio Menchik <juniomenchik@live.com>
2022-08-26 07:02:10 -0300

    master : 42. Explicando a sintaxe do Blade

--- laravel-vue/laravel-be/app/Http/Controllers/TesteController.php
+++ laravel-vue/laravel-be/app/Http/Controllers/TesteController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 class TesteController extends Controller
 {
     public function teste(int $p1,int $p2) {
-     //   echo "A soma de $p1 + $p2 eh : ".($p1+$p2);
+    //   echo "A soma de $p1 + $p2 eh : ".($p1+$p2);
     //   return view('site.teste',['p1' => $p1 , 'p2' => $p2]);
-        return view('site.teste',compact('p1','p2'));
-    }
+       return view('site.teste',compact('p1','p2'));
+       }
 }
