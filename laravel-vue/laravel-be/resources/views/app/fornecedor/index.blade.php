236e62a2bbd918164f75d65e41c54e757de977df
Jeferson Junio Menchik <juniomenchik@live.com>
2022-08-29 07:40:26 -0300

    master : 45. Blade IF/ELSE

--- laravel-vue/laravel-be/resources/views/app/fornecedor/index.blade.php
+++ laravel-vue/laravel-be/resources/views/app/fornecedor/index.blade.php
@@ -14,3 +14,11 @@
         echo 'Texto de teste';
 
 @endphp
+
+@if (count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns Fornecedores Cadastrados</h3>
+@elseif (count($fornecedores) > 10)
+    <h3>Existem vários Fornecedores Cadastrados</h3>
+@else
+    <h3>Ainda não existem Fornecedores Cadastrados</h3>
+@endif
