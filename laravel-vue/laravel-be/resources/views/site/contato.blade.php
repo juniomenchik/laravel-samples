3763a38aac96d5f4e4d0182b5c5146b437a709f4
juniomenchik <juniomenchik@live.com>
2022-10-05 15:15:10 -0300

    Recomecando curso, problemas no PHP

--- laravel-vue/laravel-be/resources/views/site/contato.blade.php
+++
@@ -1,8 +0,0 @@
-<h3>Contato (View)</h3>
-
-<ul>
-    <li><a href="{{ route('site.index') }}">Principal</a></li>
-    <li><a href="{{ route('site.sobrenos') }}">Sobre nos</a></li>
-    <li><a href="{{ route('site.contato') }}">Contato</a></li>
-</ul>
-
