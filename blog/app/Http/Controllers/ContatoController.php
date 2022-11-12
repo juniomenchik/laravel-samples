6658068a0f7ae13469a22e239dda21b8c3db8d81
juniomenchik <juniomenchik@live.com>
2022-11-12 12:41:38 -0300

    master : phpstorm configurado

--- blog/app/Http/Controllers/ContatoController.php
+++ blog/app/Http/Controllers/ContatoController.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato(){
-        return view('Site.contato');
-    }
-    }
+    public function contato() {
 
-
+        var_dump($_POST);
+        return view('site.contato', ['titulo' => 'Contato (teste)']);
+    }
+}
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -12,15 +12,30 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-/* 
+/*
 Route::get('/', function () {
-    return 'ola';
+    return 'Olá, seja bem vindo ao curso!';
 });
 */
-Route::get('/principal', [App\Http\Controllers\PrincipalController::class, 'principal']);
 
-Route::get('/sobreNos', [App\Http\Controllers\SobreNosController::class,'sobreNos']);
-    
+Route::get('/', [App\Http\Controllers\PrincipalController::class,'principal'])->name('site.index');
 
 
-Route::get('/contato',[App\Http\Controllers\ContatoController::class,'contato']);
+Route::get('/sobre-nos', [App\Http\Controllers\SobreNosController::class,'sobreNos'])->name('site.sobrenos');
+
+Route::get('/contato', [App\Http\Controllers\ContatoController::class,'contato'])->name('site.contato');
+
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){return 'Login';})->name('site.login');
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+});
+
+Route::get('/teste/{p1}/{p2}', [App\Http\Controllers\TesteController::class,'teste'])->name('site.teste');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
+});
